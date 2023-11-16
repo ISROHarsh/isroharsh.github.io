92f5535cb62cb594fa6092759dc9a133bb52fca0
ISROHarsh <gamoaster@gmail.com>
2023-11-16 13:26:01 +0530

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -1,18 +1,18 @@
-fetch("https://gh-pinned-repos.egoist.dev?username=ISROHarsh")
+fetch("https://gh-pinned-repos-api.ysnirix.xyz/api/get?username=ISROHarsh")
   .then((response) => response.json())
   .then((data) => {
-    data.forEach((repo) => {
+    data.response.forEach((repo) => {
       document.getElementsByClassName("projects")[0].innerHTML += `
                 <div class="project">
                     <div class="project-title">
-                        <a href="${repo.link}">${repo.repo}</a>
+                        <a href="${repo.repo}">${repo.name}</a>
                     </div>
                     <div class="project-description">
                         ${repo.description}
                     </div>
                     <div class="project-language">
-                        <div class="project-language-color" style="background-color: ${repo.languageColor}"></div>
-                        <div class="project-language-name">${repo.language}</div>
+                        <div class="project-language-color" style="background-color: ${repo.language.color}"></div>
+                        <div class="project-language-name">${repo.language.name}</div>
                     </div>
                 </div>
             `;
